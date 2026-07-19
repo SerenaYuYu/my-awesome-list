bc13be138187c9747fa999dd0e48061c9e698764
Liora <275142836+SerenaYuYu@users.noreply.github.com>
2026-07-19 09:15:58 +0800

    Add setup.py for my-awesome-list package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="my-awesome-list",
+      version="0.1.0",
+      packages=find_packages(),
+      python_requires=">=3.10",
+      description="Sync awesome repository sources with upstream repositories",
+      author="SerenaYuYu",
+      url="https://github.com/SerenaYuYu/my-awesome-list",
+)
